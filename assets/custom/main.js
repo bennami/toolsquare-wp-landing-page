b55727823ad3570728ae05b6ee9f0d2e58dafad8
bennami <benbenimane@hotmail.com>
2020-09-29 13:25:28 +0200

    further styling, linked some tags to see flow on mobile

---
+++ assets/custom/main.js
@@ -0,0 +1,15 @@
+
+// on scroll, change background navbar to darker color on homepage
+$(window).scroll(function(){$('nav').toggleClass('bg-not-transparent', $(this).scrollTop() > 100);});
+
+// on scroll show navbar brand
+$(window).scroll(function(){$('.navbar-brand').toggleClass('show-brand', $(this).scrollTop() > 100);});
+
+//on mobile change bg color to dar
+let btn= document.querySelector('.navbar-toggler');
+btn.addEventListener("click", function(){
+    let element = document.getElementById("navbar1");
+    element.classList.add("bg-not-transparent");
+});
+
+
